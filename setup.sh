577255bf8292584a1cff2f84e06db7a30f7fd1c9
andreykirv <andrey.kirv@gmail.com>
2021-04-16 16:07:30 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"traffic.forecast.heroku@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
