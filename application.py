0c54f3ba8099af0228e56fd6906874b152325c69
andreykirv <andrey.kirv@gmail.com>
2021-04-19 16:56:49 +0300

    Add files via upload

--- application.py
+++ application.py
@@ -3,19 +3,35 @@ import pandas as pd
 import plotly.express as px
 from traffic_forecast import make_forecast
 
+
 st.set_page_config(layout="wide")
 scatter_column, settings_column = st.beta_columns((5, 2))
 
 scatter_column.title("Traffic Forecast")
 
-settings_column.title("Settings")
+
+settings_column.header("Settings")
 settings_column.write('Use .csv file with two columns: date, sessions.')
 uploaded_file = settings_column.file_uploader("Choose File")
+settings_column.subheader('Choose prediction horizon:')
+period = settings_column.slider('', 30, 365, 365)
+settings_column.subheader('Pick seasonalities:')
+ys = settings_column.checkbox('Yearly seasonaity',True)
+ws = settings_column.checkbox('Weekly seasonality')
+settings_column.subheader('Set Uncertainty Interval Width:')
+iw = settings_column.slider('The possible range of trend', 0.8, 1.0, 0.95, 0.05)
+settings_column.subheader('It will make the trend more flexible')
+cps = settings_column.slider('', 0.05, 2.0, 0.05,0.1)
+
 
 if uploaded_file is not None:
     data_import = pd.read_csv(uploaded_file)
+
     data_import.columns = ['ds', 'y']
-    pred = make_forecast(data_import)
-    scatter_column.plotly_chart(pred)
+    pred, trend = make_forecast(data_import, period, ys, ws, iw, cps)
+    scatter_column.plotly_chart(pred, True)
+    scatter_column.title('Trends')
+    scatter_column.plotly_chart(trend, True)
+
 else:
     scatter_column.header("Please Choose a file")
--- traffic_forecast.py
+++ traffic_forecast.py
@@ -5,12 +5,93 @@ from sklearn.metrics import mean_absolute_error
 import numpy as np
 from numpy import array
 from fbprophet.plot import plot_plotly
+from fbprophet.plot import plot_components_plotly
+import plotly.graph_objs as go
+import plotly.offline as py
+from plotly.offline import init_notebook_mode
 
-def make_forecast(df):
-    model = Prophet(weekly_seasonality=False)
+def make_forecast(df, period = 365, ys = True, ws = False, iw = 0.95, cps=0.05):
+    model = Prophet(weekly_seasonality=ws,
+                    yearly_seasonality=ys,
+                    interval_width=iw,
+                    changepoint_prior_scale=cps,
+                    )
+    #model.add_country_holidays(country_name='UA')
     model.fit(df)
-    future_dates = model.make_future_dataframe(periods=365)
+    future_dates = model.make_future_dataframe(periods=period)
     prediction = model.predict(future_dates)
-    fig = plot_plotly(model, prediction)
-    fig.update_layout(title='Traffic Forecast for 365 days', yaxis_title='GA Sessions', xaxis_title="Date")
-    return fig
+
+    actual_sessions = go.Scatter(
+        name = 'Actual Sessions',
+        mode = 'markers',
+        fill=None,
+        x = list(prediction['ds']),
+        type='scatter',
+        y = list(df['y']),
+        marker=dict(
+            color='#FFBAD2',
+            line=dict(width=1))
+    )
+    trend = go.Scatter(
+        name = 'Trend',
+        mode = 'lines',
+        x = list(prediction['ds']),
+        y = list(prediction['yhat']),
+        type='scatter',
+        marker=dict(
+            color='#eb3434',
+            line=dict(width=3)
+        )
+    )
+    upper_bound = go.Scatter(
+        name = 'Upper Bound',
+        mode = 'lines',
+        x = list(prediction['ds']),
+        type='scatter',
+        y = list(prediction['yhat_upper']),
+        line= dict(color='#57b8ff')
+    )
+    lower_bound = go.Scatter(
+        name = 'Lower Bound',
+        mode = 'lines',
+        x = list(prediction['ds']),
+        type='scatter',
+        y = list(prediction['yhat_lower']),
+        line= dict(color='#57b8ff'),
+        fill='tonexty'
+    )
+    data = [upper_bound, lower_bound, actual_sessions, trend]
+    layout = dict(title=f'Traffic Forecast for {period} days',
+                yaxis_title='GA Sessions',
+                xaxis_title="Date",
+                margin=dict(l=0, r=20, t=70, b=0),
+                showlegend=True,
+                legend=dict(
+                    x=0,
+                    y=0
+                ),
+                xaxis=dict(
+                    ticklen=5,
+                    zerolinewidth=1
+                ),
+                yaxis=dict(
+                    ticklen=5,
+                    zerolinewidth=1
+                )
+            )
+
+    fig=dict(data=data,layout=layout)
+
+    fig2 = plot_components_plotly(model, prediction, figsize=(900,300))
+    fig2.update_layout(margin=dict(l=0, r=20, t=70, b=0),
+                    title='Trend of potentional growth',
+                    yaxis_title='GA Sessions',
+                    xaxis_title="Date"
+                    )
+    fig2.update_layout(margin=dict(l=0, r=20, t=70, b=0),
+                    title='Trend of potentional growth',
+                    yaxis_title='GA Sessions',
+                    xaxis_title="Date"
+                    )
+
+    return fig, fig2
